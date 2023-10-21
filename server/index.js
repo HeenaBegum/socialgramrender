1ad1ecfd519e1401b0bfffa1712f9692067839f9
HeenaBegum <80583136+HeenaBegum@users.noreply.github.com>
2023-10-20 21:26:21 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,10 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.get("/",(req,res)=>{
-res.setHeader("Access-Controll-Allow-Credentials","true");
-  res.send("API is Rinning")
-});
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
